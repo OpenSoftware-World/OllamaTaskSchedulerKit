0d9c56511f22e089bd2276833ade035af8f47831
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2025-11-23 23:45:56 +0300

    Minor edit.

--- ollamataskschedulerkit/__init__.py
+++ ollamataskschedulerkit/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Copyright© 2025 LinuxUsersLinuxMint
+""" Copyright© 2025 OpenSoftware-World
 OllamaTaskSchedulerKit Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 OllamaTaskSchedulerKit All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GitHub da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/OllamaTaskSchedulerKit
-A Copy of This Software is published on GitHub To view: https://github.com/LinuxUsersLinuxMint/OllamaTaskSchedulerKit"""
+Bu Yazılımın Bir Kopyası GitHub da yayınlanmaktadır Görüntülemek için: https://github.com/OpenSoftware-World/OllamaTaskSchedulerKit
+A Copy of This Software is published on GitHub To view: https://github.com/OpenSoftware-World/OllamaTaskSchedulerKit """
 
 from ollama import chat, ChatResponse
 import time
--- ollamataskschedulerkit/lib_ver_info.py
+++ ollamataskschedulerkit/lib_ver_info.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" Copyright© 2025 LinuxUsersLinuxMint
+""" Copyright© 2025 OpenSoftware-World
 OllamaTaskSchedulerKit Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 OllamaTaskSchedulerKit All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GitHub da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/OllamaTaskSchedulerKit
-A Copy of This Software is published on GitHub To view: https://github.com/LinuxUsersLinuxMint/OllamaTaskSchedulerKit"""
+Bu Yazılımın Bir Kopyası GitHub da yayınlanmaktadır Görüntülemek için: https://github.com/OpenSoftware-World/OllamaTaskSchedulerKit
+A Copy of This Software is published on GitHub To view: https://github.com/OpenSoftware-World/OllamaTaskSchedulerKit """
 
 PYTHON_LIB_NAME="OllamaTaskSchedulerKit"
 PYTHON_LIB_LICENCE="GPL2"
-PYTHON_LIB_IMPLEMENTED_CONTRACTS="LinuxUsersLinuxMint Privacy and Security Agreement , LinuxUsersLinuxMint Web Site Agreement"
-PYTHON_LIB_IMPLEMENTED_CONTRACTS_WEB_SITE="https://linuxuserslinuxmint.github.io/Contracts/privacyandsecutryagreement/privacyandsecutryagreement.html , https://linuxuserslinuxmint.github.io/Contracts/linuxuserslinuxmintwebsiteagreement/linuxuserslinuxmintwebsiteagreement.html"
+PYTHON_LIB_IMPLEMENTED_CONTRACTS="OpenSoftware-World Privacy and Security Agreement , OpenSoftware-World Web Site Agreement"
+PYTHON_LIB_IMPLEMENTED_CONTRACTS_WEB_SITE="https://opensoftware-world.github.io/Contracts/privacyandsecutryagreement/privacyandsecutryagreement.html , https://opensoftware-world.github.io/Contracts/linuxuserslinuxmintwebsiteagreement/linuxuserslinuxmintwebsiteagreement.html"
 PYTHON_LIB_VER="1.0"
 OLLAMA_LIB_VER="v0.5.1"
 PYTHON_LIB_SUPPORT_PLATFORM="Windows/Linux/macOS/otherOS"
 PYTHON_LIB_RELEASE_DATE="6/21/2025, Time: 21:06 / 9:06 PM"
 PYTHON_LIB_LAST_UPDATE_DATE="-/--/----, Time: --:-- / --:-- AM"
-PYTHON_LIB_AUTHOR="LinuxUsersLinuxMint"
-PYTHON_LIB_AUTHOR_WEB_SITE="https://linuxuserslinuxmint.github.io"
+PYTHON_LIB_AUTHOR="OpenSoftware-World"
+PYTHON_LIB_AUTHOR_WEB_SITE="https://opensoftware-world.github.io/"
